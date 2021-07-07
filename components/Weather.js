52d64d042c83f79de582ad64ba011c7344d88336
Kamran Ansari <crossthinking786@gmail.com>
2021-07-07 20:33:03 +0530

    increased weather icons size

--- components/Weather.js
+++ components/Weather.js
@@ -6,8 +6,8 @@ export default function Weather({ weather }) {
     <div className={styles.container} title="Pune">
       <Image
         src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-        width="60px"
-        height="60px"
+        width="80px"
+        height="80px"
         alt="Weather Icon"
       />
       <span className={styles.temp}>{Math.round(weather.main.temp)}</span>
