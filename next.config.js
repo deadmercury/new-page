b225940525dc7ab6acd78bef291a8c6915a6621a
Kamran Ansari <crossthinking786@gmail.com>
2022-09-13 17:00:28 +0530

    update npm dependencies

--- next.config.js
+++ next.config.js
@@ -1,30 +1,30 @@
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  publicExcludes: ['!**/*'],
+  buildExcludes: [/.*/],
+  disable: process.env.NODE_ENV === 'development',
+  // dynamicStartUrl: false,
+  runtimeCaching: [
+    {
+      urlPattern: /inter-v3-latin-(regular|700|500).*(\.woff2|\.woff)/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'inter font',
+      },
+    },
+    {
+      urlPattern: /oxygen-v10-latin-700.*(\.woff2|\.woff)/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'oxygen font',
+      },
+    },
+  ],
+});
 
 module.exports = withPWA({
   images: {
     domains: ['openweathermap.org'],
   },
-  pwa: {
-    dest: 'public',
-    publicExcludes: ['!**/*'],
-    buildExcludes: [/.*/],
-    disable: process.env.NODE_ENV === 'development',
-    // dynamicStartUrl: false,
-    runtimeCaching: [
-      {
-        urlPattern: /inter-v3-latin-(regular|700|500).*(\.woff2|\.woff)/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'inter font',
-        },
-      },
-      {
-        urlPattern: /oxygen-v10-latin-700.*(\.woff2|\.woff)/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'oxygen font',
-        },
-      },
-    ],
-  },
+  swcMinify: true,
 });
